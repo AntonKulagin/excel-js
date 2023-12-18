64f29ee660009e1ca33e1667493af27e814ce324
AntonKulagin <i-gorusich@mail.ru>
2023-12-18 18:45:28 +0300

    corrected template

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -19,7 +19,7 @@ function createRow(content, index) {
   return `
   <div class="table-excel__row">
     <div class="table-excel__row-info">${index ? index : ''}</div>
-    <div class="table-excel__row-data">${content ? content : ''}</div>
+    <div class="table-excel__row-data">${content}</div>
   </div>
   `
 }
@@ -28,17 +28,16 @@ function toChar(_, index) {
   return String.fromCharCode(CHAR_CODES.A + index)
 }
 
-export function createTable(rowsCount = 20) {
+export function createTable(rowsCount = 15) {
   const colsCount = CHAR_CODES.Z - CHAR_CODES.A + 1
   const rows = []
 
   const columns = new Array(colsCount).fill('').map(toChar).map(toColumn).join('')
 
-  const cells = new Array(colsCount).fill('').map(createCell).join('')
-
   rows.push(createRow(columns))
 
   for (let i = 0; i < rowsCount; i++) {
+    const cells = new Array(colsCount).fill('').map(createCell).join('')
     rows.push(createRow(cells, i + 1))
   }
 
